2aaa45f7f76d94df3b489bc282e49c4377e9f282
EdoPratamaaaaa <edoprtama152@gmail.com>
2024-09-23 00:37:42 +0700

    Add javaClass.java

---
+++ javaClass.java
@@ -0,0 +1,38 @@
+public class javaClass  {                                         // Nama Class
+    public static void main(String[] args) {                      // Main Method
+        System.out.println("Java Class");                       // Menampilkan Output
+
+        /**                                                       // Command ini untuk menonaktifkan eksekusi saat program berlangsung biasa untuk penanda
+         * nama saya :                                             
+         * alamat saya : 
+         */
+
+        System.out.println("Helloo");
+
+        // Variable dan Datatype
+        int nomor = 7 ;                                            // int = tipe data, nomor = variable_name, dan 7 = value 
+
+        // Variable
+        // Deklasi Variable
+        int angka ;
+        String nama ;
+        boolean status ;
+
+        // Inisialisasi Variable
+        angka = 1 ;
+        nama = "Amar" ;
+        status = true ;
+        System.out.println(angka + nama + status);
+
+        // Deklarasi + Inisialisasi
+
+        int angka1 = 123 ;
+        String nama1 = "Budi";
+        boolean status1 = true ;
+        System.out.println(angka1 + nama1 + status1);
+
+        // Konstanta ( Final tidak dapat diubah ) keywoard : final
+        final float phi = 3.41f ;
+    }
+    
+}
